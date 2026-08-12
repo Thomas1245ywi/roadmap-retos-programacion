5e41d708c56a678def6a7e72375213fee3be5161
Thomas1245ywi <124068425+Thomas1245ywi@users.noreply.github.com>
2026-08-12 00:19:34 -0500

    Create Thomas1245ywi.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Thomas1245ywi.java
@@ -0,0 +1,41 @@
+// 1. este es el sitio oficial de java: https://www.java.com/es/
+
+// 2. este es un comentario de una linea
+
+/**
+
+2. Este es un coemntario...
+de varias lineas
+
+**/
+
+// 3. Asi se cra una Variable
+int variable;
+
+// 4. Asi se cra una constante
+final int constante;
+
+// 5. Asi se crean variables con diferentes tipos de datos:
+
+byte edadPequena = 20; //numeros pequeños
+short Cantidad = 1000; // numeros medianos
+int edad = 20; // numeros normales
+long poblacion = 800000L; // numeros muy grandes
+
+
+float precio = 19.99f // decimal menos preciso
+double promedio = 4.5 // decimal normal
+
+char letra = 'A'; // Un solo caracter
+boolean esEstudiante = true; // true or false
+
+String nombre = "Thomas" // texto
+int[] numeros = {1, 2, 3} // arreglo de numeros
+
+
+String lenguaje = "Java";
+
+// asi se muestra en pantalla un mensaje
+System.out.println(lenguaje);
+
+  
