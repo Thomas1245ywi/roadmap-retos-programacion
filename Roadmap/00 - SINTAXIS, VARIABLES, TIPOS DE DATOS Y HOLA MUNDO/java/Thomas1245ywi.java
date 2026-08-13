8b40f13f1a44b524e7ba827e2c4da58240587d3a
Thomas1245ywi <moralesthomas44@gmail.com>
2026-08-12 23:50:33 -0500

    Ejercicio 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Thomas1245ywi.java
@@ -0,0 +1,46 @@
+// 1. este es el sitio oficial de java: https://www.oracle.com/java/
+
+// 2. este es un comentario de una linea
+
+/**
+
+2. Este es un coemntario...
+de varias lineas
+
+**/
+
+// 3. Asi se cra una Variable
+int variable;
+
+// 4. Asi se cra una constante
+// Para las constantes por nomenclatura se usan Mayusculas y Guiones Bajos
+final int CONTANTE_IMPORTANTE;
+
+// 5. Asi se crean variables con diferentes tipos de datos:
+
+byte edadPequena = 20; //numeros pequeños
+short cantidad = 1000; // numeros medianos
+int edad = 20; // numeros normales
+long poblacion = 800000L; // numeros muy grandes
+
+
+float precio = 19.99f // decimal menos preciso
+double promedio = 4.5 // decimal normal
+
+char letra = 'A'; // Un solo caracter
+boolean esEstudiante = true; // true or false
+
+String nombre = "Thomas" // texto
+int[] numeros = {1, 2, 3} // arreglo de numeros
+
+  
+public class Main {
+    public static void main(String[] args) {
+        String lenguaje = "Java";
+        //Asi se imprime en pantalla
+        System.out.println("¡Hola, " + lenguaje +"!");
+    }
+}
+
+// Java usa sitaxis camilCase
+  
